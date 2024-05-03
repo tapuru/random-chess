0417ab7ff7919473b1e51ac557df4721e99e963b
tapuru <pogladtapira@gmail.com>
2024-05-03 10:20:55 +0500

    feat(frontend): add app-select component

---
+++ client/src/shared/ui/app-select/app-select.stories.ts
@@ -0,0 +1,68 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { AppSelect, AppSelectOption, AppSelectOptionGroup } from "./app-select";
+
+const meta: Meta<typeof AppSelect> = {
+  component: AppSelect,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof AppSelect>;
+
+const basicOptions = [
+  { title: "option1", value: "o1" },
+  { title: "option2", value: "o2" },
+];
+
+export const BasicSelect: Story = {
+  args: {
+    placeholder: "basic select",
+    options: basicOptions,
+  },
+};
+
+export const UnderlinedSelect: Story = {
+  args: {
+    options: [
+      { title: "Ru", value: "ru" },
+      { title: "En", value: "en" },
+    ],
+    defaultValue: "ru",
+    variant: "underlined",
+  },
+};
+
+const groupedOptions: AppSelectOptionGroup[] = [
+  {
+    label: "group 1",
+    options: [
+      { title: "option 1", value: "o1" },
+      { title: "option 2", value: "o2" },
+      { title: "option 3", value: "o3" },
+      { title: "option 4", value: "o4" },
+    ],
+  },
+  {
+    label: "group 3",
+    options: [
+      { title: "option 5", value: "o5" },
+      { title: "option 6", value: "o6" },
+      { title: "option 7", value: "o7" },
+      { title: "option 8", value: "o8" },
+    ],
+  },
+];
+
+export const GroupedSelect: Story = {
+  args: {
+    placeholder: "grouped select",
+    optionGroups: groupedOptions,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+    placeholder: "disabled select",
+  },
+};
---
+++ client/src/shared/ui/app-select/app-select.tsx
@@ -0,0 +1,99 @@
+import * as Select from "@radix-ui/react-select";
+import cl from "./app-select.module.scss";
+import {
+  MdCheck,
+  MdKeyboardArrowDown,
+  MdKeyboardArrowUp,
+} from "react-icons/md";
+import React from "react";
+import cn from "classnames";
+
+export interface AppSelectOption extends Select.SelectItemProps {
+  title: string;
+}
+
+export interface AppSelectOptionGroup {
+  label: string;
+  options: AppSelectOption[];
+}
+
+interface AppSelectProps extends Select.SelectProps {
+  placeholder?: string;
+  withScrollButtons?: boolean;
+  options?: AppSelectOption[];
+  optionGroups?: AppSelectOptionGroup[];
+  variant?: "outlined" | "underlined";
+}
+
+export const AppSelect = ({
+  placeholder,
+  withScrollButtons,
+  options,
+  optionGroups,
+  variant,
+  ...props
+}: AppSelectProps) => {
+  return (
+    <Select.Root {...props}>
+      <Select.Trigger
+        className={cn(cl.selectTrigger, {
+          [cl.underlined]: variant === "underlined",
+        })}
+      >
+        <Select.Value placeholder={placeholder} />
+        <Select.Icon className={cl.selectIcon}>
+          <MdKeyboardArrowDown size={25} />
+        </Select.Icon>
+      </Select.Trigger>
+      <Select.Portal>
+        <Select.Content className={cl.selectContent} position="popper">
+          {withScrollButtons && (
+            <Select.ScrollUpButton className="SelectScrollButton">
+              <MdKeyboardArrowUp />
+            </Select.ScrollUpButton>
+          )}
+          <Select.Viewport className="SelectViewport">
+            {options?.map((option) => (
+              <SelectItem {...option}>{option.title}</SelectItem>
+            ))}
+            {optionGroups?.map((group, index) => (
+              <>
+                <Select.Group>
+                  <Select.Label className={cl.groupLabel}>
+                    {group.label}
+                  </Select.Label>
+                  {group.options.map((option) => (
+                    <SelectItem {...option}>{option.title}</SelectItem>
+                  ))}
+                </Select.Group>
+                {index !== optionGroups.length - 1 && (
+                  <Select.Separator className={cl.selectDivider} />
+                )}
+              </>
+            ))}
+          </Select.Viewport>
+          {withScrollButtons && (
+            <Select.ScrollDownButton className="SelectScrollButton">
+              <MdKeyboardArrowDown />
+            </Select.ScrollDownButton>
+          )}
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
+  );
+};
+
+const SelectItem = ({
+  children,
+  className,
+  ...props
+}: Select.SelectItemProps) => {
+  return (
+    <Select.Item className={cn([cl.selectItem], className)} {...props}>
+      <Select.ItemText>{children}</Select.ItemText>
+      <Select.ItemIndicator className={cl.selectItemIndicator}>
+        <MdCheck />
+      </Select.ItemIndicator>
+    </Select.Item>
+  );
+};
