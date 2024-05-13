b90d311fe09251266f1e0cc6ae7fe8b9d98e08c6
tapuru <pogladtapira@gmail.com>
2024-05-13 13:30:20 +0500

    feat(frontend): add game adn room reducers

--- client/src/app/store/store.ts
+++ client/src/app/store/store.ts
@@ -1,6 +1,11 @@
+import { gameReducer } from "@/entities/game";
+import { roomReducer } from "@/features/room";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  room: roomReducer,
+  game: gameReducer,
+});
 
 export const store = configureStore({
   reducer: rootReducer,
