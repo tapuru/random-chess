f96013ba4dda56e3377a6f60a080662cf8c921cd
tapuru <pogladtapira@gmail.com>
2024-05-13 13:29:17 +0500

    style(frontend): rename game feature to room

--- client/src/features/game/index.ts
+++
@@ -1,2 +0,0 @@
-export { gameReducer } from "./model/game-slice";
-export { LocalGameBoard } from "./ui/local-game-board/local-game-board";
--- client/src/features/game/types/game-modes.ts
+++
@@ -1,4 +0,0 @@
-export enum GameModes {
-  CLASSICAL = "classical",
-  FISHER = "fisher",
-}
