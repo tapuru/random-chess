eb44184e389a707746702795658fb6657ce8fae1
tapuru <pogladtapira@gmail.com>
2024-06-11 12:20:47 +0500

    feat(frontend): add redux-persist

--- client/src/app/store/store.ts
+++ client/src/app/store/store.ts
@@ -2,13 +2,47 @@ import { boardReducer } from "@/entities/chess-board";
 import { gameReducer } from "@/entities/game";
 import { playersReducer } from "@/entities/player";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+import storage from "redux-persist/lib/storage";
+
+const gamePersistConfig = {
+  key: "game",
+  storage: storage,
+};
+
+const boardPersistConfig = {
+  key: "board",
+  storage: storage,
+};
+
+const playersPersistConfig = {
+  key: "players",
+  storage: storage,
+};
 
 const rootReducer = combineReducers({
-  board: boardReducer,
-  game: gameReducer,
-  players: playersReducer,
+  board: persistReducer(boardPersistConfig, boardReducer),
+  game: persistReducer(gamePersistConfig, gameReducer),
+  players: persistReducer(playersPersistConfig, playersReducer),
 });
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
