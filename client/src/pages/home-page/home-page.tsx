45291d310f27ec10f2b2a2bc52779aa3b45dd882
tapuru <pogladtapira@gmail.com>
2024-06-20 16:20:34 +0500

    fix(frontend): remove unused intl provider

--- client/src/pages/home-page/home-page.tsx
+++ client/src/pages/home-page/home-page.tsx
@@ -1,16 +1,11 @@
-import { NextIntlClientProvider, useMessages } from "next-intl";
 import cl from "./home-page.module.scss";
-import { pick } from "lodash";
 import { HomeContent } from "@/widgets/home-content";
 
 export const HomePage = () => {
-  const messages = useMessages();
 
   return (
     <main className={cl.root}>
-      <NextIntlClientProvider messages={pick(messages, "HomePage")}>
         <HomeContent />
-      </NextIntlClientProvider>
     </main>
   );
 };
