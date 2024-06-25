19d5765df2ad99667bec82c96615ce74d1138289
tapuru <pogladtapira@gmail.com>
2024-06-25 13:56:50 +0500

    feat(shared): add shared types

---
+++ shared/types/game-end-reason.ts
@@ -0,0 +1,8 @@
+export type GameEndReason =
+  | "checkmate"
+  | "whiteResigned"
+  | "blackResigned"
+  | "whiteOutOfTime"
+  | "blackOutOfTime"
+  | "draw"
+  | "stalemate";
---
+++ shared/types/index.ts
@@ -0,0 +1,6 @@
+export { ChessColors } from "./chess-colors";
+export { GameModes } from "./game-modes";
+export { GameTypes } from "./game-types";
+export { GameStatus } from "./game-status";
+export { TimeControls } from "./time-controls";
+export type { GameEndReason } from "./game-end-reason";
