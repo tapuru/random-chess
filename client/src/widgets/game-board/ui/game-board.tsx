c78cc3bfab24d951bfa0fc5f220ba63777b62fc5
tapuru <pogladtapira@gmail.com>
2024-07-03 12:08:24 +0500

    refactor(frontend): refactor game board widget

--- client/src/widgets/game-board/ui/game-board.tsx
+++ client/src/widgets/game-board/ui/game-board.tsx
@@ -1,24 +1,15 @@
-"use client";
-
-import { selectGameSettings } from "@/entities/game";
 import { LocalGameBoard } from "@/features/local-game";
-import { useAppSelector } from "@/shared/lib/hooks/redux-hooks";
 import { GameTypes } from "@/shared/types/game-type";
 
-export const GameBoard = () => {
-  const gameSettings = useAppSelector(selectGameSettings);
-
-  if (!gameSettings) return null;
-
-  if (gameSettings.type === GameTypes.LOCAL) {
-    return <LocalGameBoard />;
-  }
-
-  if (gameSettings.type === GameTypes.ONLINE) {
-    return <div>online board</div>;
-  }
-
-  if (gameSettings.type === GameTypes.ENGINE) {
-    return <div>engine game</div>;
+export const GameBoard = ({ gameType }: { gameType: GameTypes }) => {
+  switch (gameType) {
+    case GameTypes.ONLINE:
+      return <div>online game board</div>;
+    case GameTypes.ENGINE:
+      return <div>engine game board</div>;
+    case GameTypes.LOCAL:
+      return <LocalGameBoard />;
+    default:
+      return null;
   }
 };
