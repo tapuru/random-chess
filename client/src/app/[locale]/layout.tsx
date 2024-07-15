7c915788ef7a8fada74f842092c1f0d953195ba2
tapuru <pogladtapira@gmail.com>
2024-07-15 19:18:57 +0500

    feat(frontend): add error success and warning colors

--- client/src/app/[locale]/layout.tsx
+++ client/src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "@/app/styles/globals.scss";
+import "react-toastify/dist/ReactToastify.css";
 import { Header } from "@/widgets/header";
 import { Providers } from "../providers/providers";
 
