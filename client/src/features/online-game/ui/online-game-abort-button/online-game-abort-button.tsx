e672e79bf4dc8a711d1dd7cb7a7bd84c7eabc5b4
tapuru <pogladtapira@gmail.com>
2024-07-17 15:52:09 +0500

    feat(frontend): add online game abort button errors handling

--- client/src/features/online-game/ui/online-game-abort-button/online-game-abort-button.tsx
+++ client/src/features/online-game/ui/online-game-abort-button/online-game-abort-button.tsx
@@ -4,9 +4,12 @@ import { selectUser } from "@/entities/auth";
 import { gameApi } from "@/entities/game";
 import { useRouter } from "@/shared/config/navigation";
 import { useAppSelector } from "@/shared/lib/hooks/redux-hooks";
+import { useHandleApiError } from "@/shared/lib/hooks/use-handle-api-error";
+import { getErrorToastConfig } from "@/shared/lib/toast-helpers";
 import { AppButton } from "@/shared/ui/app-button/app-button";
 import { skipToken } from "@reduxjs/toolkit/query";
 import { useParams } from "next/navigation";
+import { toast } from "react-toastify";
 
 export const OnlineGameAbortButton = ({ title }: { title: string }) => {
   const params = useParams<{ gameId: string }>();
@@ -14,6 +17,7 @@ export const OnlineGameAbortButton = ({ title }: { title: string }) => {
   const [abortGame, { isLoading }] = gameApi.useAbortMutation();
   const user = useAppSelector(selectUser);
   const router = useRouter();
+  const { handleApiError } = useHandleApiError();
 
   if (!game || !user) {
     return null;
@@ -28,8 +32,9 @@ export const OnlineGameAbortButton = ({ title }: { title: string }) => {
       router.push("/");
       console.log(message);
     } catch (error) {
-      //TODO: handle error
-      console.log(error);
+      handleApiError(error, (message: string) => {
+        toast.error(message, getErrorToastConfig());
+      });
     }
   };
 
